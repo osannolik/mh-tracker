3fd22880b3a55df59d74b6641aa468b7d041a094
Jonas <osannolik@godtycklig.se>
2020-03-15 14:20:14 +0100

    more robust metrics

--- mht/metrics.py
+++ mht/metrics.py
@@ -6,6 +6,9 @@ class MOTMetric(object):
     def __init__(self, ground_truth, tracks, max_d2):
         assert len(ground_truth) == len(tracks)
 
+        self.__has_truth = np.array([bool(d) for d in ground_truth]).any()
+        self.__has_hyps = np.array([bool(d) for d in tracks]).any()
+
         self.acc = mm.MOTAccumulator(auto_id=True)
 
         for t, objects in enumerate(ground_truth):
@@ -22,12 +25,57 @@ class MOTMetric(object):
                 C
             )
 
-    def MOTP(self):
+    def summary(self):
         mh = mm.metrics.create()
-        summary = mh.compute(self.acc, metrics=['motp'], return_dataframe=False)
-        return summary['motp']
+        summary = mh.compute(self.acc, metrics=mm.metrics.motchallenge_metrics, name='metrics')
+        return mm.io.render_summary(
+            summary,
+            formatters=mh.formatters,
+            namemap=mm.io.motchallenge_metric_names
+        )
+
+    def MOTP(self):
+        """
+        Multiple Object Tracking Precision
+        
+        The total position error for all matched object-hypothesis, averaged by the total
+        number of matches made. It shows the ability of the tracker to estimate precise 
+        object positions, independent of its skill at recognizing object configurations,
+        keeping consistent trajectories, etc.
+
+        returns a scalar <= 1.0
+        """
+        if self.__has_hyps and self.__has_truth:
+            mh = mm.metrics.create()
+            summary = mh.compute(self.acc, metrics=['motp'], return_dataframe=False)
+            motp = summary['motp']
+            if np.isnan(motp):
+                return 0.0
+            else:
+                return 1.0 - motp
+        elif self.__has_hyps:
+            return 0.0
+        elif self.__has_truth:
+            return 0.0
+        else:
+            return 1.0
 
     def MOTA(self):
-        mh = mm.metrics.create()
-        summary = mh.compute(self.acc, metrics=['mota'], return_dataframe=False)
-        return summary['mota']
+        """
+        Multiple Object Tracking Accuracy
+        
+        The MOT A accounts for all object configuration errors made by the tracker: 
+        false positives, misses and mismatches over all object-hypothesis assignments.
+
+        returns a scalar in (-inf, 1.0]
+        """
+        if self.__has_hyps and self.__has_truth:
+            mh = mm.metrics.create()
+            summary = mh.compute(self.acc, metrics=['mota'], return_dataframe=False)
+            return summary['mota']
+        elif self.__has_hyps:
+            return 0.0
+        elif self.__has_truth:
+            return 0.0
+        else:
+            return 1.0
