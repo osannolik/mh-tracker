75fc9a812de1f3e2109c4ea5a81b000e68416f2d
Jonas <osannolik@godtycklig.se>
2020-03-29 22:08:29 +0200

    dt for motion models

--- mht/motionmodel.py
+++ mht/motionmodel.py
@@ -1,35 +1,37 @@
 from numpy import (array, dot, sin, cos, diag, transpose)
 
+DT_DEFAULT=1.0
+
 class ConstantVelocity2D(object):
 
-    def __init__(self, T, sigma):
-        self.T = T
-        self.__Q = sigma**2 * array([
-            [T**4/4,     0, T**3/2,     0],
-            [0,     T**4/4,     0, T**3/2],
-            [T**3/2,     0,   T**2,     0],
-            [0,     T**3/2,     0,   T**2]])
+    def __init__(self, sigma):
+        self.sigma = sigma
 
     def dimension(self):
         return 4
     
-    def F(self, x):
+    def F(self, x, dt=DT_DEFAULT):
         return array([
-            [1, 0, self.T,      0],
-            [0, 1,      0, self.T],
-            [0, 0,      1,      0],
-            [0, 0,      0,      1]])
+            [1, 0, dt,  0],
+            [0, 1,  0, dt],
+            [0, 0,  1,  0],
+            [0, 0,  0,  1]
+        ])
 
-    def Q(self):
-        return self.__Q
+    def Q(self, dt=DT_DEFAULT):
+        return array([
+            [dt**4/4,      0, dt**3/2,       0],
+            [0,      dt**4/4,       0, dt**3/2],
+            [dt**3/2,      0,   dt**2,       0],
+            [0,      dt**3/2,       0,   dt**2]
+        ]) * (self.sigma**2)
 
-    def f(self, x):
-        return dot(self.F(x), x)
+    def f(self, x, dt=DT_DEFAULT):
+        return dot(self.F(x, dt), x)
 
 class CoordinatedTurn2D(object):
 
-    def __init__(self, T, sigma_vel, sigma_angle_vel):
-        self.T = T
+    def __init__(self, sigma_vel, sigma_angle_vel):
         G = array([[0, 0], [0, 0], [1, 0], [0, 0], [0, 1]])
         S = diag([sigma_vel**2, sigma_angle_vel**2])
         self.__Q = G @ S @ transpose(G)
@@ -39,21 +41,24 @@ class CoordinatedTurn2D(object):
     def dimension(self):
         return 5
 
-    def F(self, x):
+    def F(self, x, dt=DT_DEFAULT):
         return array([
-            [1, 0, self.T*cos(x[3]), -self.T*x[2]*sin(x[3]),      0],
-            [0, 1, self.T*sin(x[3]),  self.T*x[2]*cos(x[3]),      0],
-            [0, 0,                1,                      0,      0],
-            [0, 0,                0,                      1, self.T],
-            [0, 0,                0,                      0,      1]])
+            [1, 0, dt*cos(x[3]), -dt*x[2]*sin(x[3]),  0],
+            [0, 1, dt*sin(x[3]),  dt*x[2]*cos(x[3]),  0],
+            [0, 0,            1,                  0,  0],
+            [0, 0,            0,                  1, dt],
+            [0, 0,            0,                  0,  1]
+        ])
 
-    def Q(self):
+    def Q(self, dt=DT_DEFAULT):
         return self.__Q
 
-    def f(self, x):
-        dx = array([self.T*x[2]*cos(x[3]),
-                    self.T*x[2]*sin(x[3]),
-                    0,
-                    self.T*x[4],
-                    0])
+    def f(self, x, dt=DT_DEFAULT):
+        dx = dt * array([
+            x[2]*cos(x[3]),
+            x[2]*sin(x[3]),
+            0,
+            x[4],
+            0
+        ])
         return x + dx
