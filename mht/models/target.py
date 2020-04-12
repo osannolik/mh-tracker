4eda273d1286aff6f85fade4ad2d67c31db6bbc6
Jonas <osannolik@godtycklig.se>
2020-04-12 17:51:21 +0200

    implementation specific

--- mht/models/target.py
+++ mht/models/target.py
@@ -1,12 +1,4 @@
-import numpy as np
-
 from collections import (deque)
-
-from .motionmodel import (ConstantVelocity2D)
-from .measmodel import (ConstantVelocity)
-
-from mht.utils.gaussian import (Density)
-from scipy.stats.distributions import (chi2)
 
 class Target(object):
 
@@ -62,45 +54,3 @@ class Target(object):
 
     def max_coast_time(self):
         raise NotImplementedError()
-
-class TargetCV2D(Target):
-    
-    _motion = ConstantVelocity2D(sigma=0.01)
-    _measure = ConstantVelocity(sigma=0.1)
-
-    def __init__(self, density, t_now):
-        super(TargetCV2D, self).__init__(density, t_now)
-        P_G = 0.99
-        self._gating_size2 = chi2.ppf(P_G, self._measure.dimension())
-
-    @staticmethod
-    def _inv_h(z):
-        return np.array([z[0], z[1], 0.0, 0.0])
-
-    @classmethod
-    def _P0(cls):
-        R = cls._measure.R()
-        return np.diag([R[0,0], R[1,1], 1.0, 1.0])
-
-    @classmethod
-    def from_one_detection(cls, detection, t_now):
-        return cls(
-            density=Density(x=cls._inv_h(detection), P=cls._P0()),
-            t_now=t_now
-        )
-
-    @classmethod
-    def motion(self):
-        return self._motion
-    @classmethod
-    def measure(self):
-        return self._measure
-
-    def gating(self, detections):
-        return self._density.gating(detections, self._measure, self._gating_size2)
-
-    def predicted_likelihood(self, detection):
-        return self._density.predicted_likelihood(detection, self._measure)
-
-    def max_coast_time(self):
-        return 4.0
