3acd52f5a9b88d987ad9dd964147529ca13ef8e6
Jonas <osannolik@godtycklig.se>
2020-04-13 11:47:02 +0200

    exclude tests

--- setup.py
+++ setup.py
@@ -22,39 +22,9 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
     ],
-    packages=find_packages(exclude=("test",)),
-    include_package_data=False,
+    packages=find_packages(exclude=["test"]),
+    include_package_data=True,
     install_requires=[
         "numpy", "matplotlib", "motmetrics"
     ],
 )
-
-
-
-from setuptools import setup, find_packages
-
-with open('README.md') as readme_file:
-    README = readme_file.read()
-
-setup_args = dict(
-    name='mht',
-    version='0.0.1',
-    description='Track-Oriented Multiple Hypothesis Tracker',
-    long_description_content_type="text/markdown",
-    long_description=README,
-    license='MIT',
-    packages=find_packages(),
-    author='osannolik',
-    author_email='osannolik@godtycklig.se',
-    keywords=['MHT', 'MOT', 'MTT', 'Tracker'],
-    url='https://github.com/osannolik/mh-tracker/',
-    download_url='https://pypi.org/project/mh-tracker/'
-)
-
-install_requires = [
-    'elasticsearch>=6.0.0,<7.0.0',
-    'jinja2'
-]
-
-if __name__ == '__main__':
-    setup(**setup_args, install_requires=install_requires)
